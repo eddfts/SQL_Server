496691a2dcdbd46f815614e9cfbd0777070c1a57
eduardo <eddfts@gmail.com>
2018-05-30 12:03:58 -0300

    Atual.: listagem_2.1_2.sql | Desc.: Select, Distinct, Order By, Top e Percent, Over

--- listagem_2.1_2.sql
+++ listagem_2.1_2.sql
@@ -64,17 +64,44 @@ SELECT TOP (5)  orderid, orderdate, custid, empid
 FROM Sales.Orders 
 ORDER BY orderdate DESC, orderid DESC;
 
-
+/*
+*Nesta segunda query ele adiciona mais três registros depois do  registro
+* de orderid 11073. Embora em TOP Tenhamos definido apenas 5 registros.
+*/
 SELECT TOP (5) WITH TIES orderid, orderdate, custid, empid
 FROM Sales.Orders 
 ORDER BY orderdate DESC;
+
+
+--CLÁUSULA OVER
 /*
-*Nesta segunda query ele adiciona mais três registros de pois do  registro
-* de orderid 11073. Embora em TOP Tenhamos definido apenas 5 registros.
+*Opera no contexto da Cláusula Group By, Retornara somente uma linha para cada grupo.
 */
+SELECT *	    
+FROM Sales.OrderValues;
 
+SELECT orderid, custid, val, 
+       SUM(val) OVER() AS ValorTotal, --retorna o valor total de todos os registros
+	   SUM(val) OVER(PARTITION  BY custid) AS ValorTotalCust  --Retorna o valor total de todos do mesnto custid
+FROM Sales.OrderValues
 
-
+SELECT SUM (val)
+FROM Sales.OrderValues 
+--CLÁUSULA OVER FUNÇÕES: 
+/*
+ *ROW_NUMBER, RANK, DENSE_RANK  e NTILE
+ */
+SELECT orderid, custid, val,
+       ROW_NUMBER() OVER(ORDER BY val) AS rownum, -- Números inteiros sequenciais de incremento as linhas
+	   RANK()       OVER(ORDER BY val) AS rank,   --Ranque por exemplo, a linha 8 o valor(val) é o 7(sétimo) maior
+	                                              -- na tabela, ou seja rank indica quantas linhas tem valor abaixo.  
+	   DENSE_RANK() OVER(ORDER BY  val) AS dense_rank,-- O mesmo que dense porém, indica quantos valores distintos tem
+	                                                  -- abaixo.
+	   NTILE(10)    OVER(ORDER BY val) AS ntile --Como foi especificado NTILE(10) a funcão vai pegar  esse valor 
+	                                            --e dividir pelo total de linhas 830 (830/10 = 83)
+												--como OVER(Order BY val) ele vai ordenar 83 valores mais baixos 
+FROM Sales.OrderValues
+ORDER BY val;
 
 
 
