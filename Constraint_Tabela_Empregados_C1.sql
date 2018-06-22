e53695584df5249c187d560fc316d892fd80a7e4
eduardo <eddfts@gmail.com>
2018-06-22 12:21:11 -0300

    Alt.: Constraint_Tabela_Empregados_C1.sql

---
+++ Constraint_Tabela_Empregados_C1.sql
@@ -0,0 +1,22 @@
+use testedb;
+ALTER TABLE dbo.Employees
+ADD CONSTRAINT PK_Employess 
+PRIMARY KEY(empid);
+
+
+ALTER TABLE dbo.Employees 
+ADD CONSTRAINT UNQ_Employees_ssn
+UNIQUE(ssn);
+
+ALTER TABLE dbo.Employees
+ADD CONSTRAINT FK_Employees_Employess
+FOREIGN KEY(mgrid)
+REFERENCES Employees(empid);
+
+
+ALTER TABLE dbo.Employees 
+ADD CONSTRAINT CHK_Employees_salary
+CHECK (salary > 0);
+
+
+
