33218a2c812fcd56a14782f1cf601293096ca64b
eduardo <eddfts@gmail.com>
2018-06-22 12:10:15 -0300

    Alfanumericos_Collation_C2.slq

---
+++ Alfanumericos_Collation_C2.sql
@@ -0,0 +1,13 @@
+USE  TSQLFundamentals2008;
+
+SELECT name, description
+FROM sys.fn_helpcollations();
+
+SELECT empid,firstname, lastname
+FROM HR.Employees
+WHERE lastname = N'davis';
+
+  
+   
+
+
